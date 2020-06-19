8df57e02711a7d31c9078b845f3666ff51b008fd
Tim <rettink@yahoo.com>
2020-06-19 09:51:06 -0400

    keepAlive 2

--- scripts/keepAlive.sh
+++ scripts/keepAlive.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-## Detect the status of lplTradea(or the command passed in and restart it 
-## if it needs to be restarted.
+## Detect the status of lplTrade and restart if not running
 
 # Set 
 # Configure cron to Wait 10 seconds before checking
 
-user=tsk
-userPath=$(echo ~)
-lpltPath=$userPath/w/gitWS/lplTrade
-runPath=$userPath/w/gitWS/lplW/bin
+lpltPath=$HOME/git/lplTrade
+runPath=$HOME/git/venv/bin
 
 py3=$runPath/python3
 
-activate=$(. $HOME/w/gitWS/lplW/bin/activate) || echo activation failed
-CMD="$py3 $lpltPath/bin/lplt.py -o -r -c $userPath/profiles/et.json -p $lpltPath/profiles/active.json"
+activate=$(. $runPath/activate) || echo activation failed
+shScript=$HOME/bin/lplt.sh
+cmd="$py3 $lpltPath/bin/lplt.py -r -c $HOME/profiles/et.json -p $lpltPath/profiles/active.json"
+
+echo $shScript
+$shScript
 
 while true ; do
-  echo $CMD
+  echo $cmd
   PROGRAM=lplt.py
 
-  RUNNING=$(ps | grep $PROGRAM | grep -v grep) 
+  ps | grep -q $PROGRAM | grep -v grep
 
-  echo $RUNNING
-  if $RUNNING; then
+  if [ $? == 0 ]; then
      sleep 5
      continue
   fi
 
   # Resume running the program from disk
-  $($py3 $lpltPath/bin/lplt.py -o -r -c $userPath/profiles/et.json -p $lpltPath/profiles/active.json > $lpltPath/logs/tempp)
+  $py3 $lpltPath/bin/lplt.py -r -c $HOME/profiles/et.json -p $lpltPath/profiles/active.json > $lpltPath/logs/tempp
 
 done
 
